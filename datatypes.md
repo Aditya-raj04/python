622886b443f39dc55495ecada8131769500c415d
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2026-09-21 08:40:16 +0000

    python basics

---
+++ datatypes.md
@@ -0,0 +1,4 @@
+# object types/ data types
+
+-number : 1234,1.23,3+4j,0b111,decimal(), fraction()
+- String
---
+++ new.py
@@ -0,0 +1,27 @@
+from main import hello
+
+hello("kya ho rha hai")
+
+chai_one = "lemon tea"
+chai_two = "masala tea"
+
+
+x = 10
+y = x
+print(x,y)
+
+x = 10 + 10
+print(x,y)
+
+name = "aditya"
+print(len(name))
+
+myDict = {
+    "name":"aditya",
+    "age":22,
+    "course":"MCA"}
+
+print(myDict["course"])
+
+myTuple = ("aditya",22,"MCA")
+print(myTuple[0])
